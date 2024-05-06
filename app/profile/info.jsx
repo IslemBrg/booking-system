21affee43f0a3fa1da175dc868e7c1ea298adf04
Islem Bargaoui <islem.bargaoui.si@gmail.com>
2024-05-06 10:45:38 +0100

    delete reservation

--- app/profile/info.jsx
+++ app/profile/info.jsx
@@ -1,33 +1,40 @@
-import { useState, useEffect } from 'react'
-import { getDocs, query, collection, where } from 'firebase/firestore'
-import DeleteButton from './buttons/delete-button'
-import EditButton from './buttons/edit-button'
-import styles from './page.module.scss'
-import Loading from '../components/loading/loading'
+import { useState, useEffect } from "react";
+import { getDocs, query, collection, where, deleteDoc, doc } from "firebase/firestore";
+import DeleteButton from "./buttons/delete-button";
+import EditButton from "./buttons/edit-button";
+import styles from "./page.module.scss";
+import Loading from "../components/loading/loading";
+import { Button } from "@mui/material";
 
 export default function Info({ auth, session, db, setEdit }) {
-  const [reservations, setReservations] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [reservations, setReservations] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const reservationsCollection = await getDocs(
-          query(collection(db, 'reservations'), where('uid', '==', session.uid))
-        )
+          query(collection(db, "reservations"), where("uid", "==", session.uid))
+        );
         const reservationsData = reservationsCollection.docs.map((doc) => ({
           id: doc.id,
-          ...doc.data()
-        }))
-        setLoading(false)
-        setReservations(reservationsData)
+          ...doc.data(),
+        }));
+        setLoading(false);
+        setReservations(reservationsData);
       } catch (error) {
-        console.error('Error fetching data:', error)
+        console.error("Error fetching data:", error);
       }
-    }
+    };
 
-    fetchData()
-  }, [db, session.uid])
+    fetchData();
+  }, [db, session.uid]);
+
+  async function handleDelete(id) {
+    const docRef = doc(db, "reservations", id);
+    await deleteDoc(docRef);
+    window.location.reload();
+  }
 
   return (
     <>
@@ -51,17 +58,22 @@ export default function Info({ auth, session, db, setEdit }) {
         <label className={styles.left}>Reservations</label>
         <div className={styles.right}>
           {loading ? (
-            <Loading className='my-1' />
+            <Loading className="my-1" />
           ) : reservations.length > 0 ? (
             <ul>
               {reservations.map((reservation, index) => (
-                <li className='my-1 text-start' key={index}>
-                  <p className='mb-0'>ID: {reservation.id}</p>
-                  <p className='mb-0'>Date: {reservation.reservDate}</p>
-                  <p className='mb-0'>Start time: {reservation.starttime}</p>
-                  <p className='mb-0'>End time: {reservation.endtime}</p>
-                  <p className='mb-0'>Number of guests: {reservation.nbOfGuests}</p>
-                  <p className='mb-0'>Room: {reservation.suite}</p>
+                <li className="my-1 text-start" key={index}>
+                  <p className="mb-0">ID: {reservation.id}</p>
+                  <p className="mb-0">Date: {reservation.reservDate}</p>
+                  <p className="mb-0">Start time: {reservation.starttime}</p>
+                  <p className="mb-0">End time: {reservation.endtime}</p>
+                  <p className="mb-0">
+                    Number of guests: {reservation.nbOfGuests}
+                  </p>
+                  <p className="mb-0">Room: {reservation.suite}</p>
+                  <Button variant="contained" color="error" onClick={()=>handleDelete(reservation.id)}>
+                    Cancle Reservation
+                  </Button>
                 </li>
               ))}
             </ul>
@@ -71,10 +83,10 @@ export default function Info({ auth, session, db, setEdit }) {
         </div>
       </div>
 
-      <div className='my-5'>
+      <div className="my-5">
         <DeleteButton auth={auth} />
         <EditButton setEdit={setEdit} />
       </div>
     </>
-  )
+  );
 }
